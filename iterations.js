c70e42cd5f461633064f2c44f4a94f93b551cbcb
user <email@gmail.com>
2025-11-06 10:26:56 +0200

    1uzd for cikls iet

---
+++ iterations.js
@@ -0,0 +1,15 @@
+for (i = 20; i++; i <= 31) {
+    if (i < 31) {
+        console.log(i);
+    }
+}
+console.log("")
+
+let n = 30;
+let x = 21
+while (n >= x) {
+    n - 1;
+    n--;
+    console.log(n)
+}
+console.log(n)
