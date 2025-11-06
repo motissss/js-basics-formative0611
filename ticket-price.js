505832d7da0d126cb1697095d2344841a34bd2eb
user <email@gmail.com>
2025-11-06 10:32:49 +0200

    2uzd strāda

---
+++ ticket-price.js
@@ -0,0 +1,9 @@
+let visitorAge = 12;
+let isStudent = true;
+if (visitorAge < 6) {
+    console.log("Biļete ir par brīvu.");
+} if (isStudent = true) {
+    console.log("Biļete maksā 5 EUR.");
+} else {
+    console.log("Biļete maksā 10 EUR.");
+}
