0d01f48a96d1bf87f3e47555e5a565b7b8dfad4b
user <email@gmail.com>
2025-11-06 10:38:56 +0200

    4uzd gatavs

---
+++ people-array.js
@@ -0,0 +1,15 @@
+const people = [
+    {
+        name: "Andris",
+        age: "19",
+        isStudent: true,
+    },
+    {
+        name: "Peteris",
+        age: "50",
+        isStudent: false,
+    }
+]
+for (person of people) {
+    console.log("Vārds:",person.name);
+}
